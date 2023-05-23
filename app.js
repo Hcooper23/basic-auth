a6a987dd511382c7d4b4221dc47b7a5d3dcbdd0a
Hayden James Cooper <haydencpr95@gmail.com>
2023-05-22 20:14:02 -0400

    Starter-code fixed and build-out

--- app.js
+++ app.js
@@ -10,13 +10,27 @@ const { Sequelize, DataTypes } = require('sequelize');
 // Prepare the express app
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
 // Process JSON input and put the data on req.body
 app.use(express.json());
 
+app.post('/users', (req, res) => {
+  const user = req.body;
+  // Handle the user data as needed
+});
+
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
-// Process FORM intput and put the data on req.body
+// Process FORM input and put the data on req.body
 app.use(express.urlencoded({ extended: true }));
+
+app.post('/submit', (req, res) => {
+  const formData = req.body;
+  // Handle the form data as needed
+});
 
 // Create a Sequelize model
 const Users = sequelize.define('User', {
@@ -27,67 +41,62 @@ const Users = sequelize.define('User', {
   password: {
     type: DataTypes.STRING,
     allowNull: false,
-  }
+  },
 });
+
+Users.create({
+  username: 'john_doe',
+  password: 'password123',
+})
+  .then(user => {
+    console.log('User created:', user);
+  })
+  .catch(error => {
+    console.error('Error creating user:', error);
+  });
 
 // Signup Route -- create a new user
 // Two ways to test this route with httpie
 // echo '{"username":"john","password":"foo"}' | http post :3000/signup
 // http post :3000/signup username=john password=foo
 app.post('/signup', async (req, res) => {
-
   try {
     req.body.password = await bcrypt.hash(req.body.password, 10);
     const record = await Users.create(req.body);
     res.status(200).json(record);
-  } catch (e) { res.status(403).send('Error Creating User'); }
+  } catch (e) {
+    res.status(403).send('Error Creating User');
+  }
 });
-
 
 // Signin Route -- login with username and password
 // test with httpie
 // http post :3000/signin -a john:foo
 app.post('/signin', async (req, res) => {
-
-  /*
-    req.headers.authorization is : "Basic am9objpmb28="
-    To get username and password from this, take the following steps:
-      - Turn that string into an array by splitting on ' '
-      - Pop off the last value
-      - Decode that encoded string so it returns to user:pass
-      - Split on ':' to turn it into an array
-      - Pull username and password from that array
-  */
-
-  let basicHeaderParts = req.headers.authorization.split(' ');  // ['Basic', 'am9objpmb28=']
-  let encodedString = basicHeaderParts.pop();  // am9objpmb28=
-  let decodedString = base64.decode(encodedString); // "username:password"
-  let [username, password] = decodedString.split(':'); // username, password
-
-  /*
-    Now that we finally have username and password, let's see if it's valid
-    1. Find the user in the database by username
-    2. Compare the plaintext password we now have against the encrypted password in the db
-       - bcrypt does this by re-encrypting the plaintext password and comparing THAT
-    3. Either we're valid or we throw an error
-  */
   try {
+    const basicHeaderParts = req.headers.authorization.split(' '); // ['Basic', 'am9objpmb28=']
+    const encodedString = basicHeaderParts.pop(); // am9objpmb28=
+    const decodedString = base64.decode(encodedString); // "username:password"
+    const [username, password] = decodedString.split(':'); // username, password
+
     const user = await Users.findOne({ where: { username: username } });
     const valid = await bcrypt.compare(password, user.password);
     if (valid) {
       res.status(200).json(user);
-    }
-    else {
+    } else {
       throw new Error('Invalid User');
     }
-  } catch (error) { res.status(403).send('Invalid Login'); }
-
+  } catch (error) {
+    res.status(403).send('Invalid Login');
+  }
 });
 
 // make sure our tables are created, start up the HTTP server.
-sequelize.sync()
+sequelize
+  .sync()
   .then(() => {
-    app.listen(3000, () => console.log('server up'));
-  }).catch(e => {
-    console.error('Could not start server', e.message);
+    app.listen(3000, () => console.log('Server up'));
+  })
+  .catch(e => {
+    console.error('Could not start server:', e.message);
   });
